a68cea117c5d89398895ef72c157ded713256d6c
مرزوق خالد مرزوق -578 <marzouq.k911@gmail.com>
2020-07-04 17:52:03 +0300

    pokemon

---
+++ cw6/app/src/main/java/com/example/cw6/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.cw6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+cw6 marzou  = new cw6("marzouq","ahmad","hourse","7.8",);
+    }
+}
